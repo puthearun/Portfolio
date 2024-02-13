16aea0a30f75af12f01b4923c4d85b96841ad74a
Khimyaphu <128209884+puthearun@users.noreply.github.com>
2024-02-13 22:48:02 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+// Navigation bar effect on scroll
+window.addEventListener("scroll", function() {
+      const header = document.querySelector("header");
+      header.classList.toggle("sticky", window.scrollY > 0);
+});
+// Service Section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function (modalClick) {
+      serviceModals[modalClick].classList.add("active");
+}
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+      learnmoreBtn.addEventListener("click", () => {
+            modal(i);
+      });
+});
+modalCloseBtns.forEach((modalCloseBtn) => {
+      modalCloseBtn.addEventListener("click", () => { 
+            serviceModals.forEach((modalView) => {
+                  modalView.classList.remove("active");
+            });
+      });
+});
+// portfolio-list
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function (modalClick) {
+      portfolioModals[modalClick].classList.add("active");
+}
+imgCards.forEach((imgCard, i) => {
+     imgCard.addEventListener("click", () => {
+            portfolioModal(i);
+      });
+});
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+     portfolioCloseBtn.addEventListener("click", () => { 
+            portfolioModals.forEach((portfolioModalView) => {
+                  portfolioModalView.classList.remove("active");
+            });
+      });
+});
+// Our Clients
+var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+
